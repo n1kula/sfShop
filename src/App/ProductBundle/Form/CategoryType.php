1fd3bddb19b8817f23c96173579ddb3c1c2c7b76
Tomek <tomek@kulesza>
2014-11-04 20:43:44 +0100

    crud for Category created

---
+++ src/App/ProductBundle/Form/CategoryType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\ProductBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\ProductBundle\Entity\Category'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'app_productbundle_category';
+    }
+}
