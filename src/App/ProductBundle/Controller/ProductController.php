9db11afe07ba80cd2085806f7c961618d84889b3
Tomek <tomek@kulesza>
2014-11-04 20:33:10 +0100

    edit+delete products added

--- src/App/ProductBundle/Controller/ProductController.php
+++ src/App/ProductBundle/Controller/ProductController.php
@@ -31,7 +31,8 @@ class ProductController extends Controller
     		$em = $this->getDoctrine()->getManager();
     		$em->persist($product);
     		$em->flush();
-    		
+    		$this->get('session')->getFlashBag()
+    		->add('notice', 'Produkt został pomyślnie dodany');
     		return $this->redirect($this->generateUrl('product_list'));
     	}
     	
@@ -39,16 +40,59 @@ class ProductController extends Controller
                  'form' => $form->createView()
             ));    }
 
-    public function deleteAction()
+    public function deleteAction($id)
     {
-        return $this->render('AppProductBundle:Product:delete.html.twig', array(
-                // ...
-            ));    }
+        $product = $this->getDoctrine()
+    		->getRepository('AppProductBundle:Product')
+    		->find($id);
+    	
+    	if (!$product) {
+    		$this->get('session')->getFlashBag()
+    		->add('notice', 'Brak produktu');
+    	    return $this->redirect($this->generateUrl('product_list'));
+    	    
+    	}
+    	
+    	$em = $this->getDoctrine()->getManager();
+    	$em->remove($product);
+    	$em->flush();
+    	
+    	$this->get('session')->getFlashBag()
+    		->add('notice', 'Product został pomyślnie usunięty');
+    		
+        return $this->redirect($this->generateUrl('product_list'));
+    }
 
-    public function editAction()
+    public function editAction($id)
     {
+    	$request = $this->getRequest();
+    	
+    	$product = $this->getDoctrine()
+    	->getRepository('AppProductBundle:Product')
+    	->find($id);
+    	 
+    	if (!$product) {
+    		$this->get('session')->getFlashBag()
+    		->add('notice', 'Brak produktu');
+    		return $this->redirect($this->generateUrl('product_list'));
+    			
+    	}
+    	$form =$this->createForm(new ProductType(), $product, []);
+    	$form->handleRequest($request);
+    	
+    	if($form->isValid()) {
+    		$product = $form->getData();
+    		$em = $this->getDoctrine()->getManager();
+    		$em->flush();
+    		$this->get('session')->getFlashBag()->add('notice', "Produkt zostal pomyslnie zmieniony");
+    		
+    		return $this->redirect($this->generateUrl('product_list'));
+    	}
+    	
+    	
         return $this->render('AppProductBundle:Product:edit.html.twig', array(
-                // ...
-            ));    }
+               'form' => $form->createView()
+            ));
+        }
 
 }
